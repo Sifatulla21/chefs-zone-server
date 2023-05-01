20ec38e9a0cc4eaec89909f46aec822c3d5b9f96
Sifatulla21 <sifat2109@gmail.com>
2023-05-02 01:30:29 +0600

    install and check port

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.get('/', (req,res) =>{
+    res.send('Chefs zone is running')
+});
+app.listen(port, () =>{
+    console.log(`Chefs API is running on port: ${port}`)
+});
