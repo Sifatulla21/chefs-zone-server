a91b4461fa8a10d69775c4c33416e2d6c6b06c16
Sifatulla21 <sifat2109@gmail.com>
2023-05-02 20:41:14 +0600

    send all data

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const data = require('./data/chefdata.json');
 app.use(cors());
 app.get('/', (req,res) =>{
     res.send('Chefs zone is running')
 });
+app.get('/data', (req,res) =>{
+    res.send(data);
+})
 app.listen(port, () =>{
     console.log(`Chefs API is running on port: ${port}`)
 });
