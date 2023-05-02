ce7b35e290709906fca2f91d6949b19232fd7a68
Sifatulla21 <sifat2109@gmail.com>
2023-05-03 01:28:47 +0600

    rename path and match data type

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const data = require('./data/chefdata.json');
+const chef = require('./data/chefdata.json');
 app.use(cors());
 app.get('/', (req,res) =>{
     res.send('Chefs zone is running')
 });
-app.get('/data', (req,res) =>{
-    res.send(data);
+app.get('/chef', (req,res) =>{
+    res.send(chef);
 })
-app.get('/data/:id', (req,res)=>{
-    const id = req.params.id;
-    const selectedChef = data.find(chef=> chef.c_id == id);
+app.get('/chef/:id', (req,res)=>{
+    const id = parseInt(req.params.id);
+    const selectedChef = chef.find(c=>parseInt(c.c_id) === id);
     res.send(selectedChef);
 })
 app.listen(port, () =>{
